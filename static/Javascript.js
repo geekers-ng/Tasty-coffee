c7ce17076940af72cb6ec8ddb152af4e58659725
geekers-ng <gbadegesinemmanuel18@gmail.com>
2022-10-04 09:16:17 -0700

    first comitte

---
+++ static/Javascript.js
@@ -0,0 +1,9 @@
+// const Firstname='Emaanuel';
+// var surname='Gbadegsin';
+// //Firstname='Peter'
+// console.log(surname +' '+ Firstname);
+
+let a =50;
+let b =40;
+let c=a +b;
+console.log(c/2)
